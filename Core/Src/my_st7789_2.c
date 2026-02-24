bbfa80e1e69867a01270ac827767bfb2c09fa858
729dhs <hebut_gcj@163.com>
2026-02-24 16:27:34 +0800

    V1.1.1

--- Core/Src/my_st7789_2.c
+++ Core/Src/my_st7789_2.c
@@ -31,6 +31,7 @@ static void ST7789_WriteCmd(uint8_t cmd)
     HAL_SPI_Transmit(&ST7789_SPI_PORT, &cmd, 1, 1000);  // 通过SPI发送命令
 }
 
+
 /**
  * @brief 向ST7789显示屏写入一个数据字节
  * @param data 要写入的数据字节
@@ -50,10 +51,12 @@ static void ST7789_WriteData(uint8_t data)
  * @note    此函数会先将DC引脚置高（数据模式），然后通过SPI发送指定长度的数据
  *          与单个数据写入函数不同，此函数可以一次性发送多个字节的数据
  */
-static void st7789_write_data_buf(const uint8_t *data, size_t len) {
+static void st7789_write_data_buf(const uint8_t *data, size_t len)
+{
     ST7789_DC_Set();                   // 设置DC引脚，切换到数据模式
     HAL_SPI_Transmit(&hspi1, (uint8_t *)data, len, HAL_MAX_DELAY);  // 通过SPI发送数据缓冲区
 }
+
 
 /**
  * @brief ST7789显示屏初始化函数
@@ -68,4 +71,51 @@ void ST7789_Init(void)
     ST7789_RST_Set();    // 复位引脚拉高，结束复位过程
     HAL_Delay(120);
     
+    ST7789_WriteCmd(ST7789_COLMOD);
+    ST7789_WriteCmd(ST7789_COLOR_MODE_16bit);
+    ST7789_WriteCmd(0xB2);
+    {
+        // GOOD 大括号在这里用于限制作用域,确保data生命周期,尽可能减少内存占用
+        uint8_t data[] = {0x0C, 0x0C, 0x00, 0x33, 0x33};
+        st7789_write_data_buf(data, sizeof(data));
+	}
+    ST7789_SetRotation(ST7789_ROTATION);
+    
+    ST7789_WriteCmd(0xB7);
+    ST7789_WriteData(0x35);
+    ST7789_WriteCmd(0xBB);
+    ST7789_WriteData(0x19);
+    ST7789_WriteCmd(0xC0);
+    ST7789_WriteData(0x2C);
+    ST7789_WriteCmd(0xC2);
+    ST7789_WriteData(0x01);
+    ST7789_WriteCmd(0xC3);
+    ST7789_WriteData(0x12);
+    ST7789_WriteCmd(0xC4);
+    ST7789_WriteData(0x20);
+    ST7789_WriteCmd(0xC6);
+    ST7789_WriteData(0x0F);
+    ST7789_WriteCmd(0xD0);
+    ST7789_WriteData(0xA4);
+    ST7789_WriteData(0xA1);
+    
+    ST7789_WriteCmd(0xE0);
+	{
+		uint8_t data[] = {0xD0, 0x04, 0x0D, 0x11, 0x13, 0x2B, 0x3F, 0x54, 0x4C, 0x18, 0x0D, 0x0B, 0x1F, 0x23};
+		st7789_write_data_buf(data, sizeof(data));
+	}
+
+    ST7789_WriteCmd(0xE1);
+	{
+		uint8_t data[] = {0xD0, 0x04, 0x0C, 0x11, 0x13, 0x2C, 0x3F, 0x44, 0x51, 0x2F, 0x1F, 0x1F, 0x20, 0x23};
+		st7789_write_data_buf(data, sizeof(data));
+	}
+
+    ST7789_WriteCmd (ST7789_INVON);
+	ST7789_WriteCmd (ST7789_SLPOUT);
+  	ST7789_WriteCmd (ST7789_NORON);
+  	ST7789_WriteCmd (ST7789_DISPON);
+
+	HAL_Delay(50);
+	ST7789_Fill_Color(BLACK);
 }
